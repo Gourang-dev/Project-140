47b6d0c9695b4e20a5d0b85ebe8f6bdc8d026d27
Gourang-dev <77156496+Gourang-dev@users.noreply.github.com>
2021-07-25 19:28:24 +0530

    Update main.js

--- main.js
+++ main.js
@@ -21,19 +21,6 @@ var ball = {
     dy:3
 }
 
-rightWristY = 0;
-rightWristX = 0;
-scoreRightWrist = 0;
-
-game_status = "";
-
-//var doggo_theme = new Audio("https://saantonandre.github.io/doggo_theme.wav");
-
- function preload() {
-  ball_touch_paddel = loadSound("ball_touch_paddel.wav");
-  missed = loadSound("missed.wav");
-}
-
 function setup(){
 var canvas =  createCanvas(700,600);
 canvas.parent('canvas');
@@ -43,29 +30,10 @@ video.size(700, 600);
 video.hide();
 
 poseNet = ml5.poseNet(video, modelLoaded);
-poseNet.on('pose', gotPoses);
 }
 
 function modelLoaded() {
   console.log('PoseNet Is Initialized');
-}
-
-function gotPoses(results)
-{
-  if(results.length > 0)
-  {
-
-    rightWristY = results[0].pose.rightWrist.y;
-    rightWristX = results[0].pose.rightWrist.x;
-    scoreRightWrist =  results[0].pose.keypoints[10].score;
-    console.log(scoreRightWrist);
-  }
-}
-
-function startGame()
-{
-  game_status = "start";
-  document.getElementById("status").innerHTML = "Game Is Loading";
 }
 
 function draw(){
@@ -80,17 +48,7 @@ function draw(){
   fill("black");
   stroke("black");
   rect(0,0,20,700);
-
-  if(scoreRightWrist > 0.2)
-  {
-    fill("red");
-    stroke("red");
-    circle(rightWristX, rightWristY, 30);
-  }
-
-  if(game_status == "start")
-  {
-    document.getElementById("status").innerHTML = "Game Is Loaded";
+    
     //funtion paddleInCanvas call 
     paddleInCanvas();
         
@@ -220,10 +178,4 @@ function paddleInCanvas(){
   }
  
   
-}
-
-function restart()
-{
-  pcscore = 0;
-  loop();
 }
